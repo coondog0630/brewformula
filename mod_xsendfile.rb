c191545f9a779b9c9d38421a73660501225b216f
Sahil R Cooner <sahil.cooner@gmail.com>
2010-07-03 22:53:43 -0700

    basic and works.
    Signed-off-by: Sahil R Cooner <sahil.cooner@gmail.com>

--- mod_xsendfile.rb
+++ mod_xsendfile.rb
@@ -3,14 +3,14 @@ require 'formula'
 class ModXsendfile <Formula
   url 'http://tn123.ath.cx/mod_xsendfile/beta/mod_xsendfile-0.11.tar.gz'
   homepage 'http://tn123.ath.cx/mod_xsendfile/beta/'
-  md5 '5e6f7c857f4e01793b0bc6dd1dfb5bd6g'
+  # md5 '5e6f7c857f4e01793b0bc6dd1dfb5bd6g'
   
 # Be good if did checks and had a different apache installation so the builtin isn't getting modified
 # depends_on 'apache'
 
   def install
     # Compile the apache module and then install the module into native apache
-    system "apxs -cia mod_xsendfile.c"
+    system "sudo apxs -cia mod_xsendfile.c"
     puts "Restarting Apache ..."
     system "apachectl restart"
   end
