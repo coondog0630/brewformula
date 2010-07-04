7d81b24fc19103b727b8129fbe058dd3987d2e4d
Sahil R Cooner <sahil.cooner@gmail.com>
2010-07-03 22:44:43 -0700

    removed un-necessary dependencies
    Signed-off-by: Sahil R Cooner <sahil.cooner@gmail.com>

--- mod_xsendfile.rb
+++ mod_xsendfile.rb
@@ -4,8 +4,9 @@ class ModXsendfile <Formula
   url 'http://tn123.ath.cx/mod_xsendfile/beta/mod_xsendfile-0.11.tar.gz'
   homepage 'http://tn123.ath.cx/mod_xsendfile/beta/'
   md5 '5e6f7c857f4e01793b0bc6dd1dfb5bd6g'
-
- depends_on 'apache'
+  
+# Be good if did checks and had a different apache installation so the builtin isn't getting modified
+# depends_on 'apache'
 
   def install
     # Compile the apache module and then install the module into native apache
